5fb2b6a9c211afd9d8058cb758f1c3d38643374b
kb1901 <k4bhavsa@uwaterloo.ca>
2025-11-12 10:42:53 -0500

    make wave_classes an installable package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='wave_classes',
+   version='1.0',
+   description='Module with wave computing/plotting functions',
+   author='Khush Bhavsar',
+   author_email='k4bhavsa@uwaterloo.ca',
+   packages=['wave_classes']  #same as name
+)
